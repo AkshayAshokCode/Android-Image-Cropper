1b1406e9bbf78140c56afc29492e7fed59cbaf39
akshay ashok <akshayashokan1054@gmail.com>
2025-07-19 23:09:58 +0530

    #10 [ImagePicker] Add support for picking image from gallery

--- imagepicker/src/main/java/com/akshayashokcode/imagepicker/ImagePicker.kt
+++ imagepicker/src/main/java/com/akshayashokcode/imagepicker/ImagePicker.kt
@@ -1,5 +1,21 @@
 package com.akshayashokcode.imagepicker
 
-object ImagePicker {
-    fun launchImagePicker() {}
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+
+class ImagePicker(private val activity: ComponentActivity) {
+
+    private var onImagePicked: ((Uri?) -> Unit)? = null
+
+    private val galleryLauncher: ActivityResultLauncher<String> =
+        activity.registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            onImagePicked?.invoke(uri)
+        }
+
+    fun pickImageFromGallery(callback: (Uri?) -> Unit) {
+        onImagePicked = callback
+        galleryLauncher.launch("image/*")
+    }
 }
